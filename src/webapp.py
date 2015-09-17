63b29f18a39a9b05e50e49c22e1d48c8c61b3ce0
Robert Chang <robert8138@gmail.com>
2015-09-16 19:40:58 -0700

    Add more views for interacting with Database via SQLAlchemy

--- src/webapp.py
+++ src/webapp.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import render_template
 from models import *
 import sqlite3
+import datetime
 
 webapp = Flask(__name__)
 webapp.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events_all.sqlite3'
@@ -13,23 +14,30 @@ def index():
   db = sqlite3.connect("events.sqlite3")
   cur = db.execute('select * from events')
   rows = cur.fetchall()
-  return render_template('hello.html', rows = rows)
+  return render_template('index.html', rows = rows)
 
 @webapp.route('/dbdisplay')
 def display():
   return render_template("dbdisplay.html",
-                  events = Events.query.all())
+                         events = Events.query.all())
 
-@webapp.route('/users')
-def users():
-  return render_template("users.html", users = data)
+@webapp.route('/dbdisplay/<event_type>')
+def display_by_event_type(event_type):
+  return render_template("dbdisplay.html",
+                         events = Events.query.filter_by(event_type = event_type).all()) 
 
-@webapp.route('/users/<username>')
-def user(username):
-  rows = []
-  users = [user for user in data if user[-1]==username]
-  return render_template("user.html", users = users)
+@webapp.route('/dbdisplay/<year>/<month>/<day>')
+def display_by_date(year, month, day):
+  date = datetime.date(int(year), int(month), int(day))
+  return render_template("dbdisplay.html",
+                         events = Events.query.filter_by(date = date).all())   
+
+@webapp.route('/dbdisplay/duration/<duration>')
+def display_duration_greater_than(duration):
+  return render_template("dbdisplay.html",
+                         events = Events.query.filter(Events.duration > float(duration)).all())     
 
 if __name__ == '__main__':
+
   webapp.debug = True
   webapp.run()
